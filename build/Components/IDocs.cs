1fc3cd3fde5e1b1f276b4de2e4886b8dc0f0a22b
2CHEVSKII <prsroman3@gmail.com>
2024-01-24 08:21:54 +0300

    feature: Added documentation build tasks and fine tuned docfx config

---
+++ build/Components/IDocs.cs
@@ -0,0 +1,45 @@
+﻿using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DocFX;
+using Nuke.Common.Tools.DotNet;
+
+namespace Components;
+
+interface IDocs : IHazSlnFiles
+{
+    AbsolutePath DocsDirectory => RootDirectory / "docs";
+    AbsolutePath DocfxConfig => DocsDirectory / "docfx.json";
+    AbsolutePath DocsOutputDirectory => DocsDirectory / "dist";
+
+    Target Docs =>
+        _ =>
+            _.Executes(
+                () =>
+                    /*DocFXTasks.DocFXBuild(settings =>
+                        settings
+                            .SetConfigFile(DocfxConfig)
+                            .SetProcessWorkingDirectory(DocsDirectory)
+                            .SetProcessToolPath("dotnet-docfx")
+                    )*/
+                DotNetTasks.DotNet("docfx build docs/docfx.json")
+            );
+
+    Target DocsPreview =>
+        _ =>
+            _.DependsOn(Docs)
+                .Executes(
+                    () =>
+                        /*DocFXTasks.DocFXServe(settings =>
+                            settings.SetFolder(DocsDirectory / "dist").SetProcessToolPath("dotnet-docfx")
+                        )*/
+                    DotNetTasks.DotNet("docfx --serve docs/docfx.json")
+                );
+
+    Target DocsClean =>
+        _ =>
+            _.Executes(
+                () => DocsOutputDirectory.DeleteDirectory(),
+                () => (DocsDirectory / "api").DeleteDirectory()
+            );
+}
